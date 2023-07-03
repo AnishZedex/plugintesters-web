173b1bd0388d3dd3566cdee16c807ebc6227d62c
asd-05 <anishdeshpande2002@gmail.com>
2023-07-03 16:16:26 +0530

    sanity build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Blogs from "./components/Blogs"
 import Card from "./components/Card"
+import FooterCopywrite from "./components/Footer"
+import Footer from "./components/Footer"
 import Guide from "./components/Guide"
+import Navbar from "./components/Navbar"
 import OrangeRibbon from "./components/OrangeRibbon"
 import SearchPillar from "./components/SearchPillar"
 import Searchbar from "./components/Searchbar"
@@ -8,6 +11,7 @@ import Searchbar from "./components/Searchbar"
 export default function Home() {
   return (
     <main>
+      <Navbar />
         <OrangeRibbon />
         <div className="container mx-auto px-6 lg:px-52">
           <Searchbar/>
@@ -16,6 +20,9 @@ export default function Home() {
           <Card/>
           <Guide/>
         </div>
+        {/* <Footerpress/> */}
+      <Footer/>
+      <FooterCopywrite/>
     </main>
   )
 }
